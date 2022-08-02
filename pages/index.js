5f2d2f252f5e31a4669ca38e2fc06c93cb0e4df4
Kirmann Denis <kirmanndenis@gmail.com>
2022-08-02 13:31:49 +0200

    supression mdbreact

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import api from "../auth/axios";
 import PropertyVip from "../components/PropertyVip";
