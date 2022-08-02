17bc6c50875813bab6d2ba69b5683d2384e7bc97
Kirmann Denis <kirmanndenis@gmail.com>
2022-08-02 13:33:46 +0200

    sup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import "../styles/globals.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
+
 import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
